74e712ec0ce2cd685c0ca6c166ff9d8b48c14b87
CHH02 <clayhop901@gmail.com>
2026-02-11 23:28:55 -0600

    Fixed Repo name

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://chh02.github.io/FullStack-courseinfo/',
+  base: 'https://chh02.github.io/FullStack-anecdotes/',
 })
